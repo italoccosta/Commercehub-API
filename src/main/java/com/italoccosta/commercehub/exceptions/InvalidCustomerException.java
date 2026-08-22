7c55192a8fe4623644929100c0876dc4a15db21f
Italo C Costa <italo.costa13@hotmail.com>
2026-08-22 18:39:52 +0100

    feat: implement customer domain model

---
+++ src/main/java/com/italoccosta/commercehub/exceptions/InvalidCustomerException.java
@@ -0,0 +1,7 @@
+package com.italoccosta.commercehub.exceptions;
+
+public class InvalidCustomerException extends RuntimeException {
+    public InvalidCustomerException(String message) {
+        super(message);
+    }
+}
